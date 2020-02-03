2a8cf288489ba4203ce7c6f0b3032e060d05995a
elvioflauzino <elvio.flauzino@tivit.com>
2020-02-03 16:34:12 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863146;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número:  " + contaDaGabriela.numero);
+            Console.WriteLine("saldo:   " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("saldo:   " + contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03-ByteBanck/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03_ByteBanck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente ContaDaGabriel = new ContaCorrente();
+
+            ContaDaGabriel.titular = "Gabriela";
+            ContaDaGabriel.agencia = 863;
+            ContaDaGabriel.numero = 869452;
+
+            ContaCorrente ContaDaGabrielCosta = new ContaCorrente();
+
+            ContaDaGabrielCosta.titular = "Gabriela";
+            ContaDaGabrielCosta.agencia = 863;
+            ContaDaGabrielCosta.numero = 869452;
+
+            Console.WriteLine(ContaDaGabriel == ContaDaGabrielCosta);
+
+            Console.ReadLine();
+        }
+    }
+}
